262f3ded8b42ef0e378e2cc61d3dfe0ab6be140a
iyinoluwa2023 <73270351+iyinoluwa2023@users.noreply.github.com>
2021-04-24 13:17:15 -0400

    Restructured project

---
+++ string_constants.py
@@ -0,0 +1,24 @@
+YOUR_TURN = "It's 𝗬𝗢𝗨𝗥 turn!"
+MY_TURN = "It's 𝗠𝗬 turn!"
+INCORRECT_INPUT = "I don't understand that response, reply something like:" \
+                  "\nplay (number)"
+BEEN_PLAYED = "Sorry! That space has been 𝗣𝗟𝗔𝗬𝗘𝗗 𝗔𝗟𝗥𝗘𝗔𝗗𝗬, pick another one!"
+YOU_PLAYED = "𝗬𝗢𝗨 played: "
+I_PLAYED = "𝗜 played: "
+X = 'X'
+O = 'O'
+
+START_PLAY_AGAIN = "Reply with \"start\" to play again!"
+YOU_WON = ["Congrats! You 𝗪𝗢𝗡! " + START_PLAY_AGAIN,
+           "You won, congrats I guess 😒. " + START_PLAY_AGAIN,
+           "You beat me, I can admit I lost! " + START_PLAY_AGAIN,
+           "I'll beat you next time, don't you worry about it! " + START_PLAY_AGAIN]
+I_WON = ["Haha! I 𝗪𝗢𝗡! " + START_PLAY_AGAIN,
+         "I think you 𝗟𝗢𝗦𝗧, pal! " + START_PLAY_AGAIN,
+         "I think I just 𝗪𝗢𝗡! " + START_PLAY_AGAIN,
+         "You 𝗟𝗢𝗦𝗧! Better luck next time! " + START_PLAY_AGAIN]
+DRAW = ["It's a 𝗗𝗥𝗔𝗪! " + START_PLAY_AGAIN]
+
+COMMAND_BEFORE_START = "You're trying to use a  game command without starting a game! " \
+                    "Reply to this tweet with the command \"start\" to play!"
+COME_AGAIN = "Sorry to see you go! Play again soon!"
